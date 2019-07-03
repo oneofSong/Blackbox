dc0908f9b704a2c9efdc62d8df5a0e76d9929d00
wbsong <51451584+oneofSong@users.noreply.github.com>
2019-07-03 14:13:27 +0900

    pyQt

---
+++ pyQt/qDataTime.py
@@ -0,0 +1,29 @@
+from PyQt5.QtCore import QDate, Qt, QTime, QDateTime
+
+now = QDate.currentDate()
+print(now.toString())
+print(now.toString('d.M.yy'))
+print(now.toString('dd.MM.yyyy'))
+print(now.toString('ddd.MMMM.yyyy'))
+print(now.toString(Qt.ISODate))
+print(now.toString(Qt.DefaultLocaleLongDate))
+
+print("\n")
+
+
+time = QTime.currentTime()
+print(time.toString())
+print(time.toString('h.m.s'))
+print(time.toString('hh.mm.ss'))
+print(time.toString('hh.mm.ss.zzz'))
+print(time.toString(Qt.DefaultLocaleLongDate))
+print(time.toString(Qt.DefaultLocaleShortDate))
+
+print('\n')
+
+datetime = QDateTime.currentDateTime()
+print(datetime.toString())
+print(datetime.toString('d.M.yy hh:mm:ss'))
+print(datetime.toString('dd.MM.yyyy, hh:mm:ss'))
+print(datetime.toString(Qt.DefaultLocaleLongDate))
+print(datetime.toString(Qt.DefaultLocaleShortDate))
